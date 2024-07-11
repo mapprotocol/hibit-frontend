54a09dbb97fd56183c32e7a0e721dd7b8e6bf7c9
= <elongedson@gmail.com>
2024-07-11 14:15:43 +0800

    update

--- src/pages/app/Orders/index.tsx
+++ src/pages/app/Orders/index.tsx
@@ -123,19 +123,27 @@ export default function Orders({selectedCoin}: { selectedCoin: Coin | undefined 
             </div>
             <div className={styles.orders}>
                 <div className={styles.orders_title}>
+                    <div className={styles.orders_title_content}>Date</div>
                     <div className={styles.orders_title_content}>User</div>
+                    <div className={styles.orders_title_content}>Rep.</div>
                     <div className={styles.orders_title_content}>Side</div>
-                    <div className={styles.orders_title_content}>Amount</div>
-                    <div className={styles.orders_title_content}>Price(USDT)</div>
+                    <div className={styles.orders_title_content}>Total USD</div>
+                    <div className={styles.orders_title_content}>Price</div>
                     <div className={styles.orders_title_content}>Message</div>
                 </div>
                 <div className={styles.orders_area}>
                     {
                         orderLists && orderLists.map((order: any) => (
                             <div className={styles.order}>
+                                <div>
+                                    1s ago
+                                </div>
                                 <div className={styles.order_user}>
                                     <img className={styles.order_user_img} src="/images/evaluate/order.png" alt=""/>
                                     {ellipsisThree(order.base)}
+                                </div>
+                                <div>
+                                    aaa
                                 </div>
                                 {
                                     order.type == "sell" ?
